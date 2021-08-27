1d9500a0af6d2c7fa18be7f4cadd644f4fd56d84
Owen O'Connor <oroconnor@gmail.com>
2021-08-27 16:49:34 -0400

    test commit from local computer terminal

---
+++ oconnor_week1_commit.r
@@ -0,0 +1,12 @@
+# Owen O'Connor
+# Independent Study - Software Development Practices
+
+# This is a test .R file.
+
+# I have git installed on my local computer, and I am practicing adding a commit
+# to github.
+
+# I am going to try from the command terminal. And if that's successful, I will
+# try directly from RStudio, the IDE that I use for programming in R.
+
+# Let's see how it goes!
